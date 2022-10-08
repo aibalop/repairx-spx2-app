163240b5670872ac8e2a70b9f304450af7609e04
Alan Ibarra <alanibarralop@gmail.com>
2022-10-07 23:40:13 -0600

    Feat: start order-repair-form page

--- src/app/lib/modules/order-repairs/pages/order-repair-form/order-repair-form.module.ts
+++ src/app/lib/modules/order-repairs/pages/order-repair-form/order-repair-form.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { OrderRepairFormPageRoutingModule } from './order-repair-form-routing.module';
 
 import { OrderRepairFormPage } from './order-repair-form.page';
+import { SharedModule } from 'src/app/lib/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    OrderRepairFormPageRoutingModule
+    OrderRepairFormPageRoutingModule,
+    SharedModule,
+    ReactiveFormsModule
   ],
   declarations: [OrderRepairFormPage]
 })
-export class OrderRepairFormPageModule {}
+export class OrderRepairFormPageModule { }
--- src/app/lib/modules/order-repairs/pages/order-repair-form/order-repair-form.page.ts
+++ src/app/lib/modules/order-repairs/pages/order-repair-form/order-repair-form.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { EOrderRepairsRoutes } from 'src/app/lib/core/enums/modules-routes.enum';
+import { AlertDialogService } from 'src/app/lib/core/services/alert-dialog.service';
+import { ToastService } from 'src/app/lib/core/services/toast.service';
+import { OrderRepairApiService } from '../../api/order-repair.api.service';
+import { IOrderRepair } from '../../interfaces/order-repair.interface';
 
 @Component({
   selector: 'app-order-repair-form',
@@ -7,9 +15,140 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderRepairFormPage implements OnInit {
 
-  constructor() { }
+  titleHeader: string = 'Nueva Orden Reparación';
+
+  isView: boolean = false;
+
+  isEdit: boolean = false;
+
+  orderRepairId: string;
+
+  form = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    lastName: new FormControl(null, Validators.required),
+    surName: new FormControl(null),
+    phone: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+    email: new FormControl(null, Validators.email),
+    address: new FormGroup({
+      street: new FormControl(null),
+      num: new FormControl(null),
+      interiorNum: new FormControl(null),
+      colony: new FormControl(null),
+      zip: new FormControl(null),
+      location: new FormControl(null),
+      city: new FormControl(null),
+      state: new FormControl(null),
+      country: new FormControl(null)
+    }),
+  });
+
+  isSend = false;
+
+  subscription: Subscription;
+
+  constructor(
+    private _alertDialogService: AlertDialogService,
+    private _toastService: ToastService,
+    private _orderRepairApiService: OrderRepairApiService,
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute
+  ) {
+
+    this.isView = this._router.url.includes(EOrderRepairsRoutes.ORDER_REPAIRS_VIEW);
+
+    this.isEdit = this._router.url.includes(EOrderRepairsRoutes.ORDER_REPAIRS_EDIT);
+
+    this.titleHeader = this.isView ? 'Ver Orden Reparación' : this.titleHeader;
+
+    this.titleHeader = this.isEdit ? 'Editar Orden Reparación' : this.titleHeader;
+
+  }
 
   ngOnInit() {
+
+    if (this.isEdit || this.isView) {
+
+      this.subscription = this._activatedRoute.params.subscribe(params => {
+
+        this.orderRepairId = params['orderRepairId'];
+
+        if (this.orderRepairId) {
+          this._getOrderRepair();
+        } else {
+          this._router.navigate([EOrderRepairsRoutes.ORDER_REPAIRS]);
+        }
+
+      });
+
+    }
+
+  }
+
+  ngOnDestroy(): void {
+
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+
+  }
+
+  onSubmit(): void {
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      this._toastService.warning('Complete los campos obligatorios', 'Datos Incompletos');
+      return;
+    }
+
+    this.isSend = true;
+
+    if (this.isEdit) {
+      this._update();
+    } else {
+      this._create();
+    }
+
+  }
+
+  private async _getOrderRepair(): Promise<void> {
+
+    try {
+      const orderRepair = await this._orderRepairApiService.getById(this.orderRepairId).toPromise();
+      // this.form.patchValue(orderRepair); // TODO uncommented
+    } catch (error) {
+      this._toastService.danger('No se pudo obtener los datos del cliente', 'Operación Fallida');
+      this._alertDialogService.catchError(error);
+      this._router.navigate([EOrderRepairsRoutes.ORDER_REPAIRS]);
+    }
+
+  }
+
+  private async _create(): Promise<void> {
+
+    try {
+      await this._orderRepairApiService.create(this.form.value as IOrderRepair).toPromise();
+      this._toastService.success('Orden creada correctamente', 'Operación Completada');
+      this._router.navigate([EOrderRepairsRoutes.ORDER_REPAIRS]);
+    } catch (error) {
+      this.isSend = false;
+      this._toastService.danger('No se pudo completar el registro', 'Operación Fallida');
+      this._alertDialogService.catchError(error);
+    }
+
+  }
+
+  private async _update(): Promise<void> {
+
+    try {
+      await this._orderRepairApiService.update(this.orderRepairId, this.form.value as IOrderRepair).toPromise();
+      this._toastService.success('Orden actualizada correctamente', 'Operación Completada');
+      this._router.navigate([EOrderRepairsRoutes.ORDER_REPAIRS]);
+    } catch (error) {
+      this.isSend = false;
+      this._toastService.danger('No se pudo completar la actualización', 'Operación Fallida');
+      this._alertDialogService.catchError(error);
+    }
+
   }
 
 }

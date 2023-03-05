a8d88ad86276de2b3ef05ef2bd4258ef7aa6aeff
Alan Ibarra <aledibarralo@ittepic.edu.mx>
2023-03-05 02:43:30 -0700

    Feat: progress in dashboard - completed mockup

--- src/app/lib/modules/dashboard/pages/home-dashboard/home-dashboard.page.ts
+++ src/app/lib/modules/dashboard/pages/home-dashboard/home-dashboard.page.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { EDateFilterOption } from 'src/app/lib/core/enums/helpers-emuns.enum';
-import { ESettingsRoutes } from 'src/app/lib/core/enums/modules-routes.enum';
+import { EOrderRepairsRoutes, ESettingsRoutes } from 'src/app/lib/core/enums/modules-routes.enum';
 import { EOrderRepairStatus } from 'src/app/lib/core/enums/status.enum';
 import { IFilterGeneric } from 'src/app/lib/core/interfaces/filter-generic.interface';
+import { AlertDialogService } from 'src/app/lib/core/services/alert-dialog.service';
 import { SessionService } from 'src/app/lib/core/services/session.service';
+import { OrderRepairApiService } from '../../../order-repairs/api/order-repair.api.service';
+import { OrderRepair } from '../../../order-repairs/models/order-repair.model';
 
 @Component({
   selector: 'app-home-dashboard',
@@ -24,16 +28,56 @@ export class HomeDashboardPage implements OnInit {
 
   settingsRoutes = ESettingsRoutes;
 
+  lastOrders: Array<OrderRepair> = [];
+
+  ordersPendingToPay: Array<OrderRepair> = [];
+
   constructor(
     public sessionService: SessionService,
+    private _orderRepairApiService: OrderRepairApiService,
+    private _alertDialogService: AlertDialogService,
+    private _router: Router,
   ) { }
 
   ngOnInit() {
+    this._loadData();
   }
 
   onDateChange(updatedFilters: IFilterGeneric): void {
     this.filters = updatedFilters;
-    console.log('Filters: ', this.filters);
+    this._loadData();
+  }
+
+  onViewOrder(orderRepairId: string): void {
+    this._router.navigate([EOrderRepairsRoutes.ORDER_REPAIRS_VIEW, orderRepairId]);
+  }
+
+  private async _loadData(): Promise<void> {
+
+    try {
+
+      console.log('Filters: ', this.filters);
+
+      const resLastOrders = await this._orderRepairApiService.getAll(this.filters).toPromise();
+
+      this.lastOrders = resLastOrders.data.map(orderRepair => new OrderRepair(orderRepair)) as Array<OrderRepair>;
+
+      const resOrdersPendingToPay = await this._orderRepairApiService.getAll({
+        ...this.filters,
+        status: [EOrderRepairStatus.COMPLETED, EOrderRepairStatus.DELIVERED],
+        isPaid: 'false',
+      }).toPromise();
+
+      this.ordersPendingToPay = resOrdersPendingToPay.data.map(orderRepair => new OrderRepair(orderRepair)) as Array<OrderRepair>;
+
+      console.log('LastOrders: ', this.lastOrders, 'PendingToPay: ', this.ordersPendingToPay);
+
+    } catch (error) {
+
+      this._alertDialogService.catchError(error);
+
+    }
+
   }
 
 }
